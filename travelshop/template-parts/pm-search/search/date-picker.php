000e9682d2d3246dd40ea0cf77262f2b820fcb7d
Michael Amting <michaelamting@outlook.de>
2021-04-22 12:04:39 +0200

    Datepicker Clear-Button Bugfix

--- travelshop/template-parts/pm-search/search/date-picker.php
+++ travelshop/template-parts/pm-search/search/date-picker.php
@@ -14,7 +14,9 @@
         }
         ?>"/>
     </div>
-    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-x datepicker-clear" width="30" height="30" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+    <svg xmlns="http://www.w3.org/2000/svg" 
+        <?php if (!empty($_GET['pm-dr'])) { echo 'style="display: block;"'; } ?>
+        class="icon icon-tabler icon-tabler-x datepicker-clear" width="30" height="30" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
         <line x1="18" y1="6" x2="6" y2="18" />
         <line x1="6" y1="6" x2="18" y2="18" />
