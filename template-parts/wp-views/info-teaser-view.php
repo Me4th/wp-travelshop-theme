f47d665f398b7bbd77b31b8797650c002adc2026
Peter Bartl <p.bartl@pressmind.de>
2021-08-08 22:44:59 +0200

    missing class added

--- template-parts/wp-views/info-teaser-view.php
+++ template-parts/wp-views/info-teaser-view.php
@@ -8,7 +8,7 @@ $id_post_thumbnail = get_post_thumbnail_id($current_post);
 
 ?>
 <article class="col-12 col-sm-6 col-lg-3">
-    <div class="teaser">
+    <div class="teaser wp-teaser">
         <div class="teaser-image">
             <img class="w-100 h-auto" src="<?php echo wp_get_attachment_image_url($id_post_thumbnail, 'thumbnail'); ?>"
                 alt="<?php echo get_the_post_thumbnail_caption($current_post); ?>" loading="lazy" />
