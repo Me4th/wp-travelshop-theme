ca34a337f54a3a7972287199d908baaf7cd2739e
wecraft-agency <WeC2021!raft>
2021-08-31 12:59:26 +0200

    Added Reisekalender Button to Searchbar

--- template-parts/pm-search/search-bar.php
+++ template-parts/pm-search/search-bar.php
@@ -40,9 +40,9 @@ if (($total_result = get_transient( $transient)) === false) {
     <input type="hidden" name="pm-ot" value="<?php echo $id_object_type;?>">
     <div class="search-wrapper">
         <?php if(!empty($args['headline'])){?>
-        <div class="h1 text-md-center mt-0 mb-2 mb-4">
-            <?php echo $args['headline'];?>
-        </div>
+            <div class="h1 text-md-center mt-0 mb-2 mb-4">
+                <?php echo $args['headline'];?>
+            </div>
         <?php } ?>
         <div class="search-wrapper--inner search-box">
             <div class="row">
@@ -74,13 +74,30 @@ if (($total_result = get_transient( $transient)) === false) {
                 <div class="col-12 col-md-3 mb-md-0">
                     <div class="from-group mb-0">
                         <label class="d-none d-md-block">&nbsp;</label>
+                        <a class="btn btn-secondary btn-block" href="<?php echo site_url().'/calendar/'; ?>">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-calendar-event" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                <rect x="4" y="5" width="16" height="16" rx="2"></rect>
+                                <line x1="16" y1="3" x2="16" y2="7"></line>
+                                <line x1="8" y1="3" x2="8" y2="7"></line>
+                                <line x1="4" y1="11" x2="20" y2="11"></line>
+                                <rect x="8" y="15" width="2" height="2"></rect>
+                            </svg>
+                            Reisekalender
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-12 col-md-3 mb-md-0">
+                    <div class="from-group mb-0">
+                        <label class="d-none d-md-block">&nbsp;</label>
                         <a class="btn btn-primary btn-block" href="<?php echo site_url().'/'.$PMTravelShop->RouteProcessor->get_url_by_object_type(TS_TOUR_PRODUCTS).'/'; ?>">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-search" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                            <circle cx="10" cy="10" r="7" />
-                            <line x1="21" y1="21" x2="15" y2="15" />
-                        </svg> 
-                        <span class="search-bar-total-count" data-default="Suchen" data-total-count-singular="Reise" data-total-count-plural="Reisen"><?php echo empty($total_result) ? 'Suchen' : $total_result. ' Reisen'; ?></span>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-search" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                <circle cx="10" cy="10" r="7" />
+                                <line x1="21" y1="21" x2="15" y2="15" />
+                            </svg>
+                            <span class="search-bar-total-count" data-default="Suchen" data-total-count-singular="Reise" data-total-count-plural="Reisen"><?php echo empty($total_result) ? 'Suchen' : $total_result. ' Reisen'; ?></span>
                         </a>
                     </div>
                 </div>
