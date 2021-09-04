43fce5b87442ac984815736299baa5a38e6421b5
Peter Bartl <p.bartl@pressmind.de>
2021-09-04 11:33:04 +0200

    functions moved, searchbox switch added

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -105,7 +105,6 @@ jQuery(function ($) {
             }, 200, 'swing');
         }
 
-
         this.resultHandlerSearchBarStandalone = function(data, query_string, scrollto, total_result_span_id){
 
             if(total_result_span_id != null) {
@@ -122,7 +121,6 @@ jQuery(function ($) {
             }
 
         }
-
 
         this.renderWishlist = function() {
 
@@ -371,7 +369,6 @@ jQuery(function ($) {
 
         }
 
-
         this.searchbox = function () {
 
             /**
@@ -405,15 +402,352 @@ jQuery(function ($) {
 
         }
 
+        this.searchboxSwitch = function (){
+            $(".search-wrapper--tabs_btn").on('click', function (e) {
+                $(this).parents().find(".search-wrapper--tabs_btn").toggleClass('is--active');
+
+                let query_string = 'action=searchbar&pm-ot='+$(this).data('pm-ot');
+                _this.call(query_string, null, null, _this.resultHandlerSearchBar);
+
+            });
+        }
+
+        this.resultHandlerSearchBar = function(data){
+
+            for (var key in data.html) {
+                $('#' + key).html(data.html[key]);
+            }
+
+            _this.autoCompleteInit();
+            _this.dateRangePickerInit();
+            _this.initCategoryTreeSearchBarFields();
+        }
+
+        this.autoCompleteInit = function (){
+            if ($('.auto-complete').length > 0) {
+                console.log('auto autocomplete init');
+                $('.auto-complete').autocomplete({
+                    serviceUrl: '/wp-content/themes/travelshop/pm-ajax-endpoint.php?action=autocomplete',
+                    type: 'get',
+                    dataType: 'json',
+                    paramName: 'q',
+                    deferRequestBy: 0,
+                    minChars: 2,
+                    width: 'flex',
+                    groupBy: 'category',
+                    preventBadQueries: false,
+                    tabDisabled: true,
+                    preserveInput: true,
+                    onSelect: function (suggestion) {
+                        if (suggestion.data.type == 'link') {
+                            document.location.href = suggestion.data.url;
+                        } else if (suggestion.data.type == 'search') {
+                            var url = $(this).parents('form').attr('action');
+                            url += '?' + suggestion.data.search_request;
+                            document.location.href = url;
+                        }
+                    }
+                })
+            }
+        }
+
+        this.dateRangePickerInit = function (){
+            if ($('[data-type="daterange"]').length > 0) {
+
+                let easterDate = _this.theEasterDate(dayjs().year());
+                if(easterDate.isBefore(dayjs())){
+                    easterDate = _this.theEasterDate(dayjs().add(1, 'year').year());
+                }
+
+                let pfingstenDate = _this.theEasterDate(dayjs().year()).add(49, 'days');
+                if(pfingstenDate.isBefore(dayjs())){
+                    pfingstenDate = _this.theEasterDate(dayjs().add(1, 'year').year()).add(49, 'days');
+                }
+
+                let rosenmontagDate = _this.theEasterDate(dayjs().year()).subtract(48, 'days');
+                if(rosenmontagDate.isBefore(dayjs())){
+                    rosenmontagDate = _this.theEasterDate(dayjs().add(1, 'year').year()).subtract(48, 'days');
+                }
+
+
+                let picker = $('[data-type="daterange"]').daterangepicker({
+                    "ranges": {
+                        'Heute': [dayjs(), dayjs()],
+                        'Abreise in 30 Tagen': [dayjs().add(30, 'days'), dayjs().add(1, 'month')],
+                        'Abreise in 60 Tagen': [dayjs().add(60, 'days'), dayjs().add(1, 'month')],
+                        'in diesem Monat': [dayjs().startOf('month'), dayjs().endOf('month')],
+                        'über Rosenmontag': [rosenmontagDate.subtract(7, 'days'), rosenmontagDate],
+                        'über Ostern': [easterDate.subtract(7, 'days'), easterDate],
+                        'über Pfingsten': [pfingstenDate.subtract(7, 'days'), pfingstenDate],
+                        'über Weihnachten': [dayjs().date(15).month(11), dayjs().date(24).month(11)],
+                        'über Silvester': [dayjs().date(25).month(11), dayjs().date(31).month(11)],
+                        'im nächsten Monat': [dayjs().add(1, 'month').startOf('month'), dayjs().add(1, 'month').endOf('month')],
+                    },
+                    "showWeekNumbers": false,
+                    "autoUpdateInput": false,
+                    "alwaysShowCalendars": true,
+                    "showDropdowns": true,
+                    "minDate": $('[data-type="daterange"]').data('mindate'),
+                    "maxDate": $('[data-type="daterange"]').data('maxdate'),
+                    "showCustomRangeLabel": false,
+                    // "autoApply": true,
+                    "locale": {
+                        "format": "DD.MM.YYYY",
+                        "separator": " - ",
+                        "applyLabel": "Auswahl übernehmen",
+                        "fromLabel": "Von",
+                        "toLabel": "Bis",
+                        "customRangeLabel": "Custom",
+                        "weekLabel": "W",
+                        "daysOfWeek": [
+                            "So",
+                            "Mo",
+                            "Di",
+                            "Mi",
+                            "Do",
+                            "Fr",
+                            "Sa"
+                        ],
+                        "monthNames": [
+                            "Januar",
+                            "Februar",
+                            "März",
+                            "April",
+                            "Mai",
+                            "Juni",
+                            "Juli",
+                            "August",
+                            "September",
+                            "Oktober",
+                            "November",
+                            "Dezember"
+                        ],
+
+                        "firstDay": 1,
+                        "buttonClasses": "btn btn-outline-secondary btn-block",
+                        "applyButtonClasses": "btn btn-outline-secondary btn-block",
+                        "cancelClass": "btn-default",
+                        "cancelLabel": '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-x" width="30" height="30" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><line x1="18" y1="6" x2="6" y2="18" /><line x1="6" y1="6" x2="18" y2="18" /></svg>'
+                    },
+                    /*
+                    "startDate": dayjs().startOf('hour'),
+                    "endDate": dayjs().startOf('hour').add(64, 'hour')
+                    */
+                }, function (start, end, label) {
+                    if ($(window).width() <= 767) {
+                        $([document.documentElement, document.body]).animate({
+                            scrollTop: $('.travelshop-datepicker').offset().top - 83
+                        }, 500);
+                    }
+                    //console.log('New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD') + ' (predefined range: ' + label + ')');
+                });
+
+
+                $('[data-type="daterange"]').on('apply.daterangepicker', function (ev, picker) {
+
+                    $(this).val(picker.startDate.format('DD.MM.') + ' - ' + picker.endDate.format('DD.MM.YYYY'));
+
+                    // build the a pm ready query string
+                    $(this).data('value', picker.startDate.format('YYYYMMDD') + '-' + picker.endDate.format('YYYYMMDD'));
+
+                    $(this).trigger('change');
+                });
+
+
+                $('[data-type="daterange"]').on('cancel.daterangepicker', function (ev, picker) {
+                    $(this).val('');
+                    $(this).data('value', '');
+                    $(this).trigger('change');
+                });
+
+                document.addEventListener("DOMContentLoaded", function (event) {
+                    $('.travelshop-datepicker-input').on('click', function () {
+                        $('.daterangepicker select').prettyDropdown({
+                            height: 30
+                        });
+                    });
+                    $('.monthselect').on('change', function () {
+                        $('.daterangepicker select').prettyDropdown({
+                            height: 30
+                        });
+                    });
+                });
+
+                // -- show/hide clear button in datepicker
+                $('.travelshop-datepicker-input').on('change', function (e) {
+                    if ($(e.target).val() != '') {
+                        $(e.target).parent().siblings('.datepicker-clear').show();
+                    } else {
+                        $(e.target).parent().siblings('.datepicker-clear').hide();
+                    }
+                });
+
+            }
+        }
+
+        /**
+         * based on this magic date (easter sunday), it's possible to calculate some special dates:
+         *
+         * offset to german feiertag from the easterdate
+         * - Weiberfastnacht	-52
+         * - Rosenmontag	-48
+         * - Fastnachtsdienstag	-47
+         * - Aschermittwoch	-46
+         * - Gründonnerstag	-3
+         * - Karfreitag	-2
+         * - Ostersonntag	0
+         * - Ostermontag	+1
+         * - Christi Himmelfahrt	+39
+         * - Pfingstsonntag	+49
+         * - Pfingstmontag	+50
+         * - Fronleichnam	+60
+         *
+         * @param Y Year YYYY
+         * @returns dayjs
+         */
+        this.theEasterDate = function (Y) {
+            var C = Math.floor(Y/100);
+            var N = Y - 19*Math.floor(Y/19);
+            var K = Math.floor((C - 17)/25);
+            var I = C - Math.floor(C/4) - Math.floor((C - K)/3) + 19*N + 15;
+            I = I - 30*Math.floor((I/30));
+            I = I - Math.floor(I/28)*(1 - Math.floor(I/28)*Math.floor(29/(I + 1))*Math.floor((21 - N)/11));
+            var J = Y + Math.floor(Y/4) + I + 2 - C + Math.floor(C/4);
+            J = J - 7*Math.floor(J/7);
+            var L = I - J;
+            var M = 3 + Math.floor((L + 40)/44);
+            var D = L + 28 - 31*Math.floor(M/4);
+
+            return dayjs().date(D).month(M - 1).year(Y);
+        }
+
+
+        /**
+         * Adds the multi item select feature to the the default bootstrap dropdown box
+         */
+        this.initCategoryTreeSearchBarFields = function(){
+
+            if ($('.dropdown-menu-select').length > 0) {
+
+                // -- prevent dropdown close when clicked inside
+                $('.dropdown-menu-select').on('click', function (e) {
+                    e.stopPropagation();
+                });
+
+                $('.dropdown-menu-select .filter-prompt').on('click', function (e) {
+                    e.preventDefault();
+
+                    $(this).parents('.dropdown').find('.dropdown-toggle').trigger('click');
+
+                    e.stopPropagation();
+                })
+
+                // -- make dropdown span-checkboxes clickable
+                $('.multi-level-checkboxes .form-check span').on('click', function (e) {
+                    if ($(e.target).siblings('input').is(':checked')) {
+                        $(e.target).siblings('input').prop('checked', false).trigger('change');
+                    } else {
+                        $(e.target).parent().find('input').prop('checked', true).trigger('change');
+                    }
+                });
+
+
+                // -- create label text on input change, put it into span
+                $('.dropdown-menu-select').find('input').on('change', function (e) {
+
+                    var placeHolderTag = $(this).parents('.dropdown').find('.selected-options'),
+                        placeHolderDefaultText = placeHolderTag.data('placeholder'),
+                        placeHolderGetText = placeHolderTag.text(),
+                        placeHolderOptionsText = '',
+                        that = $(this);
+
+                    if (placeHolderGetText != placeHolderDefaultText) {
+                        placeHolderOptionsText = placeHolderGetText;
+                    }
+
+                    var thatValue = that.parent().find('> label').text();
+                    thatValue = $.trim(thatValue);
+
+                    var allBoxes = $(this).parent().parent().find('input');
+                    var allEmpty = true;
+
+                    $(allBoxes).each(function (key, input) {
+                        if (input.checked) {
+                            allEmpty = false;
+                        }
+                    });
+
+                    // function to hide/show the clear-button
+                    if (!allEmpty) {
+                        $(this).parent().parent().parent().parent().find('.dropdownReiseziel .dropdown-clear').show();
+                    } else {
+                        $(this).parent().parent().parent().parent().find('.dropdownReiseziel .dropdown-clear').hide();
+                    }
+
+                    if (that.prop('checked') === true) {
+                        if (placeHolderOptionsText != '') {
+                            placeHolderOptionsText = placeHolderOptionsText + ', ' + thatValue;
+                        } else {
+                            placeHolderOptionsText = thatValue;
+                        }
+                    } else {
+                        if (placeHolderGetText.indexOf(',') != -1) {
+                            if (placeHolderGetText.indexOf(thatValue) == 0) {
+                                placeHolderOptionsText = placeHolderOptionsText.replace(thatValue + ', ', '');
+                            } else {
+                                placeHolderOptionsText = placeHolderOptionsText.replace(', ' + thatValue, '');
+                            }
+                        } else {
+                            placeHolderOptionsText = $.trim(placeHolderDefaultText);
+                        }
+                    }
+
+                    placeHolderTag.text(placeHolderOptionsText);
+
+                });
+
+                $('.dropdown-clear').on('click', function (e) {
+                    e.stopPropagation();
+                    var placeHolderTag = $(e.target).parent().parent().find('.selected-options');
+                    var dropdown = $(e.target).parent().parent().parent().find('.dropdown-menu');
+                    var allBoxes = $(dropdown).find('input');
+
+                    $(allBoxes).each(function (key, input) {
+                        if (input.checked) {
+                            $(input).prop('checked', false).trigger('change');
+                        }
+                    });
+
+                    $(placeHolderTag).empty().text('bitte wählen');
+
+                    $(e.target).hide();
+                });
+
+                // Init on load
+                $('.dropdown-menu-select input:checked').trigger('change');
+
+            }
+
+
+
+        }
+
+        this.init = function(){
+            _this.renderWishlist();
+            _this.wishlistEventListeners();
+            _this.wishListInit();
+            _this.pagination();
+            _this.searchbox();
+            _this.searchboxSwitch();
+            _this.filter();
+            _this.autoCompleteInit();
+            _this.dateRangePickerInit();
+            _this.initCategoryTreeSearchBarFields();
+        }
+
     };
 
     var Search = new TSAjax('/wp-content/themes/travelshop/pm-ajax-endpoint.php');
-    Search.renderWishlist();
-    Search.wishlistEventListeners();
-    Search.wishListInit();
-    Search.pagination();
-    Search.searchbox();
-    Search.filter();
-
+    Search.init();
 });
 
