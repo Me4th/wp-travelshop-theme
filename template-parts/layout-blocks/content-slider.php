077c2cf33b4ebbb33e45cbcf3413f999df4589d6
Peter Bartl <p.bartl@pressmind.de>
2021-09-10 17:53:31 +0200

    slider bugfix and improvements

--- template-parts/layout-blocks/content-slider.php
+++ template-parts/layout-blocks/content-slider.php
@@ -10,16 +10,16 @@
  *             'image' => get_stylesheet_directory_uri().'/assets/img/slide-1.jpg',
  *             'image_alt_tag' => '',
  *             'btn_link' => '/blog/',
- *             'btn_link_target' => '', enum(_blank, _self)
+ *             'btn_link_target' => '', // enum(_blank, _self)
  *             'btn_label' => 'Weiterlesen'
  *          ],
  *          [
  *              'type' => 'product',
  *              'pm-id' => '869696'
  *              'image_type' => '', // enum(from_product, custom)
- *              'image' => get_stylesheet_directory_uri().'/assets/img/slide-1.jpg',
+ *              'image' => get_stylesheet_directory_uri().'/assets/img/slide-1.jpg', // img src if image_type is 'custom'
  *              'image_alt_tag' => '',
- *              'image_number' => 0,
+ *              'image_number' => 0, // image number from product image list if type is 'from_product'
  *          ]
  *      ]
  *  ];
@@ -30,11 +30,44 @@
 if (empty($args)) {
     return;
 }
-?>
 
+$slide_items = [];
+foreach ($args['items'] as $item) {
+
+    $image_url = '/placeholder.svg?wh=800x800&text=image not set';
+    $image_alt_tag = '';
+    if ($item['type'] == 'product') {
+        if(empty((int)$item['pm-id'])){
+            continue;
+        }
+
+        $item['mo'] = new \Pressmind\ORM\Object\MediaObject((int)$item['pm-id']);
+        if(empty($item['mo']->id) === true){
+            continue;
+        }
+        $item['moc'] = $item['mo']->getDataForLanguage(TS_LANGUAGE_CODE);
+        if($item['image_type'] == 'from_product' || empty($item['image_type'])){
+            if (is_array($item['moc']->bilder_default)) {
+                if(!isset($item['moc']->bilder_default[$item['image_number']])){
+                    $item['image_number'] = 0;
+                }
+                $item['image'] = $item['moc']->bilder_default[$item['image_number']]->getUri('detail');
+                $item['image_alt_tag'] = $item['moc']->bilder_default[$item['image_number']]->copyright();
+            } elseif (is_string($item['moc']->bilder_default)) {
+                $item['image'] = SITE_URL . "/wp-content/themes/travelshop/assets/img/placeholder.svg.php?wh=250x170&text=" . urlencode($item['moc']->bilder_default);
+            }
+        }
+    }
+
+    $slide_items[] = $item;
+}
+
+
+
+?>
 <section class="content-block content-block-content-slider">
     <?php
-    if (count($args['items']) > 1) {
+    if (count($slide_items) > 1) {
         ?>
         <div class="content-slider--nav">
             <button class="prev-button">
@@ -57,39 +90,11 @@ if (empty($args)) {
     ?>
     <div class="content-slider--inner">
         <?php
-        foreach ($args['items'] as $item) {
-
-            $image_url = '/placeholder.svg?wh=800x800&text=image not set';
-            $image_alt_tag = '';
-            if ($item['type'] == 'product') {
-                if(empty((int)$item['pm-id'])){
-                    continue;
-                }
-
-                $mo = new \Pressmind\ORM\Object\MediaObject((int)$item['pm-id']);
-                $moc = $mo->getDataForLanguage(TS_LANGUAGE_CODE);
-                if($item['image_type'] == 'from_product'){
-                    if (is_array($moc->bilder_default)) {
-                        if(!isset($moc->bilder_default[$item['image_number']])){
-                            $item['image_number'] = 0;
-                        }
-                        $image_url = $moc->bilder_default[$item['image_number']]->getUri('detail');
-                        $image_alt_tag = $moc->bilder_default[$item['image_number']]->copyright();
-                    } elseif (is_string($moc->bilder_default)) {
-                        $image_url = SITE_URL . "/wp-content/themes/travelshop/assets/img/placeholder.svg.php?wh=250x170&text=" . urlencode($moc->bilder_default);
-                    }
-                }
-            }
-
-            if (!empty($item['image']) && ($item['type'] == 'content' || $item['image_type'] == 'custom')) {
-                $image_url = $item['image'];
-                $image_alt_tag = $item['image_alt_tag'];
-            }
-
+        foreach ($slide_items as $item) {
             ?>
             <article class="content-slider--item content-slider--item__<?php echo $item['type']; ?>">
                 <div class="content-slider--image">
-                    <div style="background-image: url('<?php echo $image_url; ?>');"></div>
+                    <div style="background-image: url('<?php echo $item['image']; ?>');"></div>
                 </div>
                 <div class="content-slider--content">
                     <div class="container">
@@ -111,7 +116,7 @@ if (empty($args)) {
                             </div>
                             <?php
                         } elseif ($item['type'] == 'product') {
-                            echo $mo->render('Teaser6', TS_LANGUAGE_CODE);
+                            echo $item['mo']->render('Teaser6', TS_LANGUAGE_CODE);
                         }
                         ?>
                     </div>
