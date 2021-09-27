c47dfdaa63177461251609a07ee789ef0e0960f7
Peter Bartl <p.bartl@pressmind.de>
2021-09-27 21:00:27 +0200

    mongo change

--- template-parts/pm-search/result-pagination.php
+++ template-parts/pm-search/result-pagination.php
@@ -1,21 +1,32 @@
 <?php
 /**
- * @var int $page_size
- * @var int $current_page
- * @var int $pages
+ * <code>
+ * $args = ['total_result' => 100,
+ *            'current_page' => 1,
+ *            'pages' => 10,
+ *            'page_size' => 10,
+ *            'cache' => [
+ *              'is_cached' => false,
+ *              'info' => []
+ *            ],
+ *            'items' => [],
+ *            'mongodb' => [
+ *              'aggregation_pipeline' => ''
+ *            ]
+ *           ];
+ * </code>
+ * @var array $args
  */
-
 ?>
-
 <section class="content-block content-block-pagination">
     <div class="row">
         <div class="col-12">
             <nav aria-label="">
                 <ul class="ajax-enabled-pagination pagination">
 
-                    <li class="page-item<?php echo ($current_page == 1) ? ' disabled' : ''; ?>">
+                    <li class="page-item<?php echo ($args['current_page'] == 1) ? ' disabled' : ''; ?>">
                         <a class="page-link"
-                           href="?action=search&<?php echo BuildSearch::getCurrentQueryString($current_page - 1, $page_size); ?>">
+                           href="?action=search&<?php echo BuildSearch::getCurrentQueryString($args['current_page'] - 1, $args['page_size']); ?>">
                             <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-chevron-left"
                                  width="16" height="16" viewBox="0 2 24 24" stroke-width="2" stroke="#607D8B"
                                  fill="none" stroke-linecap="round" stroke-linejoin="round">
@@ -26,29 +37,27 @@
                     </li>
                     <?php
                     // prevent result from overloading
-                    if ($current_page > $pages) {
-                        $current_page = $pages;
+                    if ($args['current_page'] > $args['pages']) {
+                        $args['current_page'] = $args['pages'];
                     }
-
                     // show max five numerated pagination buttons
                     $from = 1;
-                    $to = $pages;
-                    if ($pages > 5) {
-                        $to = ceil($current_page / 5) * 5;
-                        $to = $to > $pages ? $pages : $to;
+                    $to = $args['pages'];
+                    if ($args['pages'] > 5) {
+                        $to = ceil($args['current_page'] / 5) * 5;
+                        $to = $to > $args['pages'] ? $args['pages'] : $to;
                         $from = $to - 5 + 1;
                     }
-
                     for ($page = $from; $page <= $to; $page++) {
                         ?>
-                        <li class="page-item<?php echo ($current_page == $page) ? ' active' : ''; ?>"><a
+                        <li class="page-item<?php echo ($args['current_page'] == $page) ? ' active' : ''; ?>"><a
                                     class="page-link"
-                                    href="?action=search&<?php echo BuildSearch::getCurrentQueryString($page, $page_size); ?>"><?php echo $page; ?></a>
+                                    href="?action=search&<?php echo BuildSearch::getCurrentQueryString($page, $args['page_size']); ?>"><?php echo $page; ?></a>
                         </li>
                     <?php } ?>
-                    <li class="page-item<?php echo ($current_page == $pages) ? ' disabled' : ''; ?>"><a
+                    <li class="page-item<?php echo ($args['current_page'] == $args['pages']) ? ' disabled' : ''; ?>"><a
                                 class="page-link"
-                                href="<?php echo ($current_page >= $pages) ? '#' : '?action=search&' . BuildSearch::getCurrentQueryString($current_page + 1, $page_size); ?>">
+                                href="<?php echo ($args['current_page'] >= $args['pages']) ? '#' : '?action=search&' . BuildSearch::getCurrentQueryString($args['current_page'] + 1, $args['page_size']); ?>">
                             <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-chevron-right"
                                  width="16" height="16" viewBox="0 2 24 24" stroke-width="2" stroke="#607D8B"
                                  fill="none" stroke-linecap="round" stroke-linejoin="round">
