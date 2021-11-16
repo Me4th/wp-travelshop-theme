85baa591470f1aac976c09daa3b429f3059ae956
wecraft-agency <WeC2021!raft>
2021-11-16 16:01:47 +0100

    Menu active change

--- functions/menus.php
+++ functions/menus.php
@@ -108,8 +108,6 @@ function activeIds(array &$elements, $currentId)
 {
     $breadcrumb = array();
 
-    print_r($_SERVER['REQUEST_URI']);
-
     foreach ( $elements as &$element ) {
         if ( $element->object_id == $currentId ) {
             $parentId = $element->menu_item_parent;
