56f0f858723edc9e0d9b25f7fa91057b5a5ee982
Peter Bartl <p.bartl@pressmind.de>
2021-11-24 20:47:11 +0100

    bugfix

--- pagebuilders/beaverbuilder/modules/pm-search-bar/includes/frontend.php
+++ pagebuilders/beaverbuilder/modules/pm-search-bar/includes/frontend.php
@@ -6,5 +6,6 @@
 $args = (array)$settings;
 unset($args['class']); // resolve naming conflict (we remove the beaver builder custom class)
 $args['class'] = $args['color_scheme'];
+$args['id_object_type'] = TS_TOUR_PRODUCTS;
 
 load_template_transient(get_template_directory() . '/template-parts/layout-blocks/search-bar.php', false,  $args);
