7dfef9c4ec97051c87b9976c30cc1f246927187f
Peter Bartl <p.bartl@pressmind.de>
2021-12-08 20:05:45 +0100

    fix

--- pagebuilders/beaverbuilder/modules/pm-search-header/includes/frontend.php
+++ pagebuilders/beaverbuilder/modules/pm-search-header/includes/frontend.php
@@ -1,6 +1,12 @@
 <?php
+use \Pressmind\Travelshop\Search;
+
 /**
  * @var object $settings defined by beaver builder module
  */
-load_template_transient(get_template_directory() . '/template-parts/layout-blocks/search-header.php', false,  (array)$settings);
-load_template_transient(get_template_directory() . '/template-parts/layout-blocks/search-header.php', false,  (array)$settings);
+
+$args = (array)$settings;
+unset($args['class']); // resolve naming conflict (we remove the beaver builder custom class)
+$args = array_merge($args, Search::getResult(['pm-ot' => TS_TOUR_PRODUCTS],2, 12, true, false));
+
+load_template_transient(get_template_directory() . '/template-parts/layout-blocks/search-header.php', false,  (array)$args);
