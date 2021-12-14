8d0eb08054bfe0aa4a7e95fea086195fc1739348
we craft <moin@wecraft.agency>
2021-12-14 10:04:20 +0100

    replaced total count variable

--- template-parts/layout-blocks/product-teaser.php
+++ template-parts/layout-blocks/product-teaser.php
@@ -50,9 +50,6 @@ $moreResultsLink = '/mehr-reisen-link/';
             <div class="col-12 col-md-auto pb-4">
                 <a href="<?php echo $moreResultsLink; ?>" title="<?php echo str_replace('[TOTAL_RESULT]', $result['total_result'], $args['link_top_text']);?>" class="btn btn-primary btn-further">
                     <?php echo str_replace('[TOTAL_RESULT]', $result['total_result'], $args['link_top_text']);?>
-                    <span class="icon">
-                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 407.437 407.437" style="enable-background:new 0 0 407.437 407.437;" xml:space="preserve"> <polygon fill="#ffffff" points="386.258,91.567 203.718,273.512 21.179,91.567 0,112.815 203.718,315.87 407.437,112.815 "></polygon></svg>
-                    </span>
                 </a>
             </div>
         </div>
@@ -95,9 +92,6 @@ $moreResultsLink = '/mehr-reisen-link/';
         <div class="col-12 text-center pb-4">
             <a href="<?php echo $moreResultsLink; ?>" title="<?php echo str_replace('[TOTAL_RESULT]', $result['total_result'], $args['link_bottom_text']);?>" class="btn btn-primary btn-further">
                 <?php echo str_replace('[TOTAL_RESULT]', $result['total_result'], $args['link_bottom_text']);?>
-                <span class="icon">
-                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 407.437 407.437" style="enable-background:new 0 0 407.437 407.437;" xml:space="preserve"> <polygon fill="#ffffff" points="386.258,91.567 203.718,273.512 21.179,91.567 0,112.815 203.718,315.87 407.437,112.815 "></polygon></svg>
-                </span>
             </a>
         </div>
     </div>
