51fb4c04c634635f21ee01116542923b6103440b
we craft <moin@wecraft.agency>
2021-12-16 14:09:01 +0100

    first-init template category header

--- template-parts/layout-blocks/category-header.php
+++ template-parts/layout-blocks/category-header.php
@@ -31,7 +31,7 @@
         <div class="category-header-content-wrapper content-header-vertical--<?php echo $args['content_alignment_vertical']; ?> content-header-horizontal--<?php echo $args['content_alignment_horizontal']; ?>">
             <div class="container">
                 <article class="category-header-content category-header-content--<?php echo $args['content_box_type']; ?>">
-                    <h1 class="category-header-title" <?php if ( !empty($args['headline_color']) ) { ?>style="color: <?php if ( strlen($args['headline']) < 8 ) { ?>#<?php } ?><?php echo $args['headline_color']; ?>"<?php } ?>>
+                    <h1 class="category-header-title" <?php if ( !empty($args['headline_color']) ) { ?>style="color: <?php if ( strlen($args['headline_color']) < 8 ) { ?>#<?php } ?><?php echo $args['headline_color']; ?>"<?php } ?>>
                         <?php echo $args['headline']; ?>
                     </h1>
 
