0544727e6ebb5987eeec0fa70b37dd01dbe33f24
we craft <moin@wecraft.agency>
2021-12-28 22:23:22 +0100

    changed fields beaverbuilder module

--- pagebuilders/beaverbuilder/modules/category-header/includes/frontend.php
+++ pagebuilders/beaverbuilder/modules/category-header/includes/frontend.php
@@ -5,4 +5,8 @@
 $args = (array)$settings;
 $args['background_overlay_gradient'] = FLBuilderColor::gradient($settings->background_overlay_gradient);
 
+echo "<pre>";
+    var_dump($args);
+echo "</pre>";
+
 load_template_transient(get_template_directory() . '/template-parts/layout-blocks/category-header.php', false,  $args);
