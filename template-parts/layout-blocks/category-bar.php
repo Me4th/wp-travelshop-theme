2ad3f795583c50104919dccf3518ac61de4ccd45
Michael Amting <michaelamting@outlook.de>
2022-02-09 07:24:05 +0100

    Insert links to category-bar

--- template-parts/layout-blocks/category-bar.php
+++ template-parts/layout-blocks/category-bar.php
@@ -3,26 +3,26 @@
     <h2>Weitere Zielgebiete für Fernreisen</h2>
 
     <div class="category-bar-items">
-        <div class="category-bar-item">
+        <a href="#" class="category-bar-item">
             <strong>Abu Dhabi</strong>
             <small>ab € 547</small>
-        </div>
-        <div class="category-bar-item">
+        </a>
+        <a href="#" class="category-bar-item">
             <strong>Kuba</strong>
             <small>ab € 1.161</small>
-        </div>
-        <div class="category-bar-item">
+        </a>
+        <a href="#" class="category-bar-item">
             <strong>Afrika</strong>
             <small>ab € 1.843</small>
-        </div>
-        <div class="category-bar-item">
+        </a>
+        <a href="#" class="category-bar-item">
             <strong>Sri Lanka</strong>
             <small>ab € 933</small>
-        </div>
-        <div class="category-bar-item">
+        </a>
+        <a href="#" class="category-bar-item">
             <strong>Seychellen</strong>
             <small>ab € 1.347</small>
-        </div>
+        </a>
     </div>
     
 </div>
