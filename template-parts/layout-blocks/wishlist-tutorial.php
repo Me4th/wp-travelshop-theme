13808d21826045b44ab407a12bee20ebe149c0f9
Michael Amting <michaelamting@outlook.de>
2022-02-13 03:45:49 +0100

    Content Edit

--- template-parts/layout-blocks/wishlist-tutorial.php
+++ template-parts/layout-blocks/wishlist-tutorial.php
@@ -20,24 +20,13 @@
         <div class="wishlist-tutorial-block">
             <img class="block-image" src="https://images.pexels.com/photos/3487734/pexels-photo-3487734.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=300" alt="Alt Texts" />
             <div class="block-content">
-                <h2>Immer parat</h2>
+                <h2>Reisen sammeln</h2>
                 <p> 
-                    Ihr Merkzettel befindet sich im Header.
-                    Die Zahl neben dem Herz zeigt Ihnen an, wie viele
-                    Lieblingsreisen Sie sich bereits gemerkt haben.
+                    Durch das Klicken auf das Herz in der Reise
+                    Übersicht oder in der Reise Detailansicht,
+                    können Sie Reisen zu Ihrem Merkzettel hinzufügen.
                 </p>
             </div>
-        </div>
-        <div class="wishlist-tutorial-block">
-            <div class="block-content">
-                <h2>Immer parat</h2>
-                <p> 
-                    Ihr Merkzettel befindet sich im Header.
-                    Die Zahl neben dem Herz zeigt Ihnen an, wie viele
-                    Lieblingsreisen Sie sich bereits gemerkt haben.
-                </p>
-            </div>
-            <img class="block-image" src="https://images.pexels.com/photos/5990737/pexels-photo-5990737.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=300" alt="Alt Texts" />
         </div>
     </div>
 
