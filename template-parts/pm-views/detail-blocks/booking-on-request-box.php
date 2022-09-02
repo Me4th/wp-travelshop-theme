8c46cdf2534e54835c5ce02623dfad75e0536d07
Me4th <Me4th@users.noreply.github.com>
2022-09-02 06:08:44 +0000

    Sync from template@dd8f55e881376e88c6e33754ab82e8d584906ad0

--- template-parts/pm-views/detail-blocks/booking-on-request-box.php
+++ template-parts/pm-views/detail-blocks/booking-on-request-box.php
@@ -16,8 +16,12 @@ if($args['booking_on_request'] === false){
 ?>
 <div class="row">
     <div class="col">
-        <p>Diese Reise ist zur Zeit nicht online buchbar.</p>
-        <p>Bitte senden Sie uns stattdessen eine <a href="#">Anfrage</a> oder nutzen Sie unseren telefonischen Kundenservice: <a href="tel://<?php echo do_shortcode('[ts-company-hotline]');?>"><?php echo do_shortcode('[ts-company-hotline]');?></a></p>
-        <button class="btn btn-primary">Anfrage</button>
+        <div class="booking-request">
+            <div class="alert alert-danger">
+                Diese Reise ist zur Zeit nicht online buchbar.
+            </div>
+            <p>Bitte senden Sie uns stattdessen eine <a href="#">Anfrage</a> oder nutzen Sie unseren telefonischen Kundenservice: <a href="tel://<?php echo do_shortcode('[ts-company-hotline]');?>"><?php echo do_shortcode('[ts-company-hotline]');?></a></p>
+            <button class="btn btn-primary">Anfrage</button>
+        </div>
     </div>
 </div>
